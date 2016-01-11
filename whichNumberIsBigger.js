31003731e993cb47163f8ab44646780f44f08b5b
kshiao13 <kshiao13@gmail.com>
2016-01-11 12:42:06 -0800

    Added the numerical comparison program.

---
+++ whichNumberIsBigger.js
@@ -0,0 +1,29 @@
+function addFocus(e){
+	var eventTarget=e.target;
+	var eventTargetID=e.target.id;
+	if (eventTargetID==='a' || eventTargetID==='a'){
+		eventTarget.focus();
+	}
+}
+
+function whichNumberIsBigger(){
+	var firstNumber=numberA.value;
+	var secondNumber=numberB.value;
+	if (firstNumber!=secondNumber){
+		if (numberA>numberB) {
+			document.getElementById('response').textContent=firstNumber;
+		} else {
+			document.getElementById('response').textContent=secondNumber;
+		}
+	} else {
+		document.getElementById().textContent='Please enter 2 different numbers.';
+	}
+}
+
+var numberA=document.getElementById('a');
+var numberB=document.getElementById('b');
+numberA.addEventListener('click',addFocus,false);
+numberB.addEventListener('click',addFocus,false);
+
+var submitButton=document.getElementsByTagName('button')[0];
+submitButton.addEventListener('click',whichNumberIsBigger,false);
